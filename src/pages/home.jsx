3246b86a214cd4700889ae066f2af0874b6661c8
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-05-16 13:48:54 +0530

    Built Trending GIF pages

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { GifState } from '../context/gif-context'
+import Gif from '../components/gif'
+
 
 const Home = () => {
+    const { gf, filter, gifs, setgifs, } = GifState()
+
+    const fetchingTrenfingGif = async () => {
+        const { data } = await gf.trending({
+            limit: 20,
+            type: filter,
+            rating: "g"
+        })
+
+        setgifs(data)
+    }
+
+    useEffect(() => {
+        fetchingTrenfingGif()
+    }, [filter])
+
     return (
-        <div>Home</div>
+        <div>
+            <img
+                src='/banner.gif'
+                alt='banner'
+                className='w-full mt-5'
+            />
+            {/* FilterGif */}
+
+
+            <div className='columns-2 md:col-span-3 lg:columns-4 xl:columns-5 gap-2'>
+
+                {gifs.map((gif) => (
+                    <Gif gif={gif} key={gif.id} />
+                ))}
+
+            </div>
+        </div>
     )
 }
 
