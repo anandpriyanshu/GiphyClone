b0baf2e0d02e867722eceb3bd744d35394513d07
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-05-18 11:55:07 +0530

    Bulit category page

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { GifState } from '../context/gif-context'
+import Gif from '../components/gif'
+import FollowOn from '../components/follwoe-on'
 
 const Category = () => {
+    const [searchResults, setSearchResults] = useState([]);
+
+    const { gf } = GifState()
+
+    const { category } = useParams()
+
+    const fetchResults = async () => {
+        const { data } = await gf.gifs(category, category);
+
+        setSearchResults(data);
+    };
+
+
+    useEffect(() => {
+        fetchResults();
+    }, [category]);
+
+
+
     return (
-        <div>Category</div>
+        <div className='flex flex-col  sm:flex-row gap-5 my-4'>
+
+            <div className='w-full sm:w-72'>
+
+                {searchResults.length > 0 && <Gif gif={searchResults[0]} hover={false} />}
+                <span className='text-gray-600 text- pt-2'>
+                    Don&apos;t tell it to me
+                </span>
+
+
+                <FollowOn />
+
+                <div className='divider'>
+
+                </div>
+
+            </div>
+            <div>
+                <h2 className='text-4xl pb-1 font-extrabold capitalize'>{category.split("-").join(" & ")} Gifs</h2>
+                <h2 className='text-lg text-gray-400 pb-3 font-bold hover:text-gray-50 cursor-pointer'>@{category}</h2>
+                {searchResults.length > 0 && (
+                    <div className="columns-2 md:columns-3 lg:columns-4 gap-2">
+                        {searchResults.slice(1).map((gif) => (
+                            <Gif gif={gif} key={gif.id} />
+                        ))}
+                    </div>
+                )}
+
+            </div>
+
+        </div >
     )
 }
 
